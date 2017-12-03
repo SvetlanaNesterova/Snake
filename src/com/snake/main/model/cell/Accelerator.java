cd27f15fcdbe80832f964aabd503ac860871f57a
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2017-12-03 14:58:29 +0500

    Accelerator, Retarder, Exit created

---
+++ src/com/snake/main/model/cell/Accelerator.java
@@ -0,0 +1,13 @@
+package com.snake.main.model.cell;
+
+import com.snake.main.model.Snake;
+
+public class Accelerator extends Food {
+    public Accelerator(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void makeEffect(Snake snake) {
+    }
+}
---
+++ src/com/snake/main/model/cell/Retarder.java
@@ -0,0 +1,13 @@
+package com.snake.main.model.cell;
+
+import com.snake.main.model.Snake;
+
+public class Retarder extends Food{
+    public Retarder(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void makeEffect(Snake snake) {
+    }
+}
